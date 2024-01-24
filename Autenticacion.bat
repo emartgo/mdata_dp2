69b99c259a8fb648f62ca2a923a1b20665489c2d
rafapm21 <rafaperezmorales@gmail.com>
2024-01-24 22:20:15 +0100

    Actualización autenticacion

--- Autenticacion.bat
+++ Autenticacion.bat
@@ -6,3 +6,6 @@ gcloud services enable dataflow.googleapis.com
 gcloud services enable pubsub.googleapis.com
 gcloud services enable vision.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
+python -m venv dataproject2
+dataproject2\Scripts\activate.bat
+pip install -U -r requirements.txt
